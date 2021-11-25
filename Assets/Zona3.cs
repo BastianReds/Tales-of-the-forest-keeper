f22b9d1ffac7b42815505b69663169862870ee72
Sebastian Rojas Benavides <bastian210@hotmail.com>
2021-11-25 13:49:45 -0500

    Initial commit

---
+++ Assets/Zona3.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Zona3 : MonoBehaviour
+{
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            SceneManager.LoadScene(4);
+        }
+    }
+}
+
